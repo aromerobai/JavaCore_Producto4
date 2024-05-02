c85e314dc60be439c2c3c5fe232f463bde82a4d4
aromerobai <aromerobai@uoc.edu>
2024-05-02 15:25:50 +0200

    Añado endpoints a ApiEndPoints.

--- src/main/java/org/javacoreuocx/alquilatusvehiculos/controller/ApiEndPoints.java
+++ src/main/java/org/javacoreuocx/alquilatusvehiculos/controller/ApiEndPoints.java
@@ -3,12 +3,12 @@ package org.javacoreuocx.alquilatusvehiculos.controller;
 import org.javacoreuocx.alquilatusvehiculos.model.Cliente;
 import org.javacoreuocx.alquilatusvehiculos.model.Oficina;
 import org.javacoreuocx.alquilatusvehiculos.repository.ClienteRepository;
-import org.javacoreuocx.alquilatusvehiculos.repository.ContratoAlquilerRepository;
 import org.javacoreuocx.alquilatusvehiculos.repository.OficinaRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.beans.factory.annotation.Autowired;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -22,16 +22,22 @@ public class ApiEndPoints {
     @Autowired
     private ClienteRepository clienteRepository;
 
-    @Autowired
-    private ContratoAlquilerRepository contratoAlquilerRepository;
     @GetMapping("/oficinas")
-    public List<Oficina> getAllOficinas() {
-        return oficinaRepository.findAll();
-    }
+    public List<Oficina> getAllOficinas() {return oficinaRepository.findAll();}
 
     @GetMapping("/clientes")
     public List<Cliente> getAllClientes() {
         return  clienteRepository.findAll();
     }
+
+    @GetMapping("/oficinasCiudad")
+    public Optional<Oficina> getOficinasId(String ciudad) {return oficinaRepository.findByCiudad(ciudad);}
+
+    @GetMapping("/clientesNombre")
+    public Cliente getAllClientes(String username) {
+        return  clienteRepository.findByUsername(username);
+    }
+    @GetMapping("/oficinasCodigoPostal")
+    public Optional<Oficina> getAllOficinas(String codigoPostal) {return oficinaRepository.findByCodigoPostal(codigoPostal);}
 
 }
